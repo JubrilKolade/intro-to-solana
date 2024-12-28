3226aac0b13b5a97d72f7ed7a606e857d20f05d0
JubrilKolade <jubrilkolade97@gmail.com>
2024-12-28 02:11:40 +0000

    initial commit

---
+++ client/client.ts
@@ -0,0 +1,32 @@
+import * as web3 from "@solana/web3.js";
+// Manually initialize variables that are automatically defined in Playground
+const PROGRAM_ID = new web3.PublicKey("2xG9fSAwe69VpGTjYQmnN8Y7ym9bCZtU6uUAfb8pQJV6");
+const connection = new web3.Connection("https://api.devnet.solana.com", "confirmed");
+const wallet = { keypair: web3.Keypair.generate() };
+
+// Client code...
+console.log(PROGRAM_ID.toString());
+
+// Get latest blockhash info
+const blockhashInfo = await connection.getLatestBlockhash();
+
+// Create transaction
+const tx = new web3.Transaction({
+  ...blockhashInfo,
+});
+
+// Add our hello world program instruction
+tx.add(
+  new web3.TransactionInstruction({
+    programId: PROGRAM_ID,
+    keys: [],
+    data: Buffer.from([]),
+  })
+);
+
+// Sign transaction
+tx.sign(wallet.keypair);
+
+// Send the transaction to the Solana cluster
+const txHash = await connection.sendRawTransaction(tx.serialize());
+console.log(txHash);
